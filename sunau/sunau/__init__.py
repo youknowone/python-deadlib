18f41ff1b632ecbd6e8473fc3ea053af04922a44
Luke Blaney <git@lukeblaney.co.uk>
2024-10-19 18:00:05 +0100

    output deprecation warning in sunau module

--- sunau/sunau/__init__.py
+++ sunau/sunau/__init__.py
@@ -106,8 +106,8 @@ is destroyed.
 from collections import namedtuple
 import warnings
 
-# python-deadlib: Remove deprecation warning
-#warnings._deprecated(__name__, remove=(3, 13))
+# python-deadlib: Replace deprecation warning not to raise exception
+warnings.warn(f"{__name__} was removed in Python 3.13. Please be aware that you are currently NOT using standard '{__name__}', but instead a separately installed 'standard-{__name__}'.", DeprecationWarning)
 
 
 _sunau_params = namedtuple('_sunau_params',
