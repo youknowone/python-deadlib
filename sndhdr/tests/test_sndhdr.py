bb79b9ddd0a5e9828a55d90f752c472514ed6c54
Luke Blaney <git@lukeblaney.co.uk>
2024-10-21 21:32:43 +0100

    Add sndhdrdata directory for sndhdr tests Copied from cpython 3.12 Fixes https://github.com/youknowone/python-deadlib/issues/20

--- sndhdr/tests/test_sndhdr.py
+++ sndhdr/tests/test_sndhdr.py
@@ -18,7 +18,7 @@ class TestFormats(unittest.TestCase):
             ('sndhdr.voc', ('voc', 0, 1, -1, 8)),
             ('sndhdr.wav', ('wav', 44100, 2, 5, 16)),
         ):
-            filename = findfile(filename, subdir="sndhdrdata")
+            filename = findfile(filename, subdir="tests/sndhdrdata")
             what = sndhdr.what(filename)
             self.assertNotEqual(what, None, filename)
             self.assertSequenceEqual(what, expected)
@@ -29,7 +29,7 @@ class TestFormats(unittest.TestCase):
             self.assertEqual(what.sampwidth, expected[4])
 
     def test_pickleable(self):
-        filename = findfile('sndhdr.aifc', subdir="sndhdrdata")
+        filename = findfile('sndhdr.aifc', subdir="tests/sndhdrdata")
         what = sndhdr.what(filename)
         for proto in range(pickle.HIGHEST_PROTOCOL + 1):
             dump = pickle.dumps(what, proto)
